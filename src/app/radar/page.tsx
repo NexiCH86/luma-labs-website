e729a0e3d1903b6b28b4eee5ee6c9a81efd26b28
NexiCH86 <luis.moran@mail.ch>
2026-08-19 16:40:57 +0200

    Mount aircraft card in LuMa Radar

--- src/app/radar/page.tsx
+++ src/app/radar/page.tsx
@@ -1,6 +1,12 @@
 import RadarClient from "./RadarClient";
+import AircraftCard from "./AircraftCard";
 import "./radar.css";
 
 export default function RadarPage() {
-    return <RadarClient />;
+    return (
+        <>
+            <RadarClient />
+            <AircraftCard />
+        </>
+    );
 }
