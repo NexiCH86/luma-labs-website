4fd7954c67e26ff141940b055a5910c62470d848
NexiCH86 <luis.moran@mail.ch>
2026-08-19 17:11:10 +0200

    Enable live flight intelligence

--- src/app/radar/page.tsx
+++ src/app/radar/page.tsx
@@ -1,11 +1,13 @@
 import RadarClient from "./RadarClient";
 import AircraftCard from "./AircraftCard";
+import LiveFlightIntelligence from "./LiveFlightIntelligence";
 import "./radar.css";
 
 export default function RadarPage() {
     return (
         <>
             <RadarClient />
+            <LiveFlightIntelligence />
             <AircraftCard />
         </>
     );
